126428cfaa0c881717013e1af4c7981c8dc5d5e8
DeadEye <supharuekchai.i@ku.th>
2020-04-19 15:02:37 +0700

    debug staff from

---
+++ final_cafe/ViewAllSales.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace final_cafe
+{
+    public partial class ViewAllSales : Form
+    {
+        public ViewAllSales()
+        {
+            InitializeComponent();
+        }
+
+        private void ViewAllSales_Load(object sender, EventArgs e)
+        {
+            DataAccess _DataAccess = new DataAccess();
+
+            foreach (Details SaleDetails in _DataAccess.RetreiveAllSales())
+            {
+                SalesGridView.Rows.Add(SaleDetails.SaleID, SaleDetails.SaleTime, SaleDetails.Name, SaleDetails.Total, "View Products");
+            }
+        }
+
+        private void SalesGridView_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (e.RowIndex >= 0)
+            {
+                if (SalesGridView.Columns[e.ColumnIndex].Name == "ProductsColumn")
+                {
+                    int SaleID = Convert.ToInt32(SalesGridView.Rows[e.RowIndex].Cells["SaleIDColumn"].Value);
+
+                    ViewSaleItems _ViewSaleItems = new ViewSaleItems(SaleID);
+
+                    _ViewSaleItems.ShowDialog();
+                }
+            }
+        }
+    }
+}
