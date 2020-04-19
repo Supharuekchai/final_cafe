54965d94deb1fc5e22038f9771324845be68cec2
AsadaYoon <loveyou060842@gmail.com>
2020-04-19 16:02:08 +0700

    add all

---
+++ final_cafe/Customer_Signup.Designer.cs
@@ -0,0 +1,227 @@
+﻿namespace final_cafe
+{
+    partial class Customer_Signup
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Customer_Signup));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.cus_name = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.cus_gender = new System.Windows.Forms.ComboBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.cus_type = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.cus_telno = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.back = new System.Windows.Forms.PictureBox();
+            this.bt_sign = new System.Windows.Forms.Button();
+            this.bt_cancer = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.back)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
+            resources.ApplyResources(this.pictureBox1, "pictureBox1");
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.TabStop = false;
+            // 
+            // cus_name
+            // 
+            resources.ApplyResources(this.cus_name, "cus_name");
+            this.cus_name.Name = "cus_name";
+            // 
+            // label1
+            // 
+            resources.ApplyResources(this.label1, "label1");
+            this.label1.Name = "label1";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.cus_gender);
+            resources.ApplyResources(this.groupBox1, "groupBox1");
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.TabStop = false;
+            // 
+            // cus_gender
+            // 
+            resources.ApplyResources(this.cus_gender, "cus_gender");
+            this.cus_gender.FormattingEnabled = true;
+            this.cus_gender.Items.AddRange(new object[] {
+            resources.GetString("cus_gender.Items"),
+            resources.GetString("cus_gender.Items1")});
+            this.cus_gender.Name = "cus_gender";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.cus_type);
+            resources.ApplyResources(this.groupBox2, "groupBox2");
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.TabStop = false;
+            // 
+            // cus_type
+            // 
+            resources.ApplyResources(this.cus_type, "cus_type");
+            this.cus_type.FormattingEnabled = true;
+            this.cus_type.Items.AddRange(new object[] {
+            resources.GetString("cus_type.Items"),
+            resources.GetString("cus_type.Items1"),
+            resources.GetString("cus_type.Items2")});
+            this.cus_type.Name = "cus_type";
+            // 
+            // label2
+            // 
+            resources.ApplyResources(this.label2, "label2");
+            this.label2.Name = "label2";
+            // 
+            // cus_telno
+            // 
+            resources.ApplyResources(this.cus_telno, "cus_telno");
+            this.cus_telno.Name = "cus_telno";
+            // 
+            // label3
+            // 
+            resources.ApplyResources(this.label3, "label3");
+            this.label3.Name = "label3";
+            // 
+            // textBox1
+            // 
+            resources.ApplyResources(this.textBox1, "textBox1");
+            this.textBox1.Name = "textBox1";
+            // 
+            // label4
+            // 
+            resources.ApplyResources(this.label4, "label4");
+            this.label4.Name = "label4";
+            // 
+            // textBox2
+            // 
+            resources.ApplyResources(this.textBox2, "textBox2");
+            this.textBox2.Name = "textBox2";
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.label4);
+            this.groupBox3.Controls.Add(this.textBox2);
+            this.groupBox3.Controls.Add(this.label3);
+            this.groupBox3.Controls.Add(this.textBox1);
+            resources.ApplyResources(this.groupBox3, "groupBox3");
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackColor = System.Drawing.Color.Transparent;
+            resources.ApplyResources(this.pictureBox2, "pictureBox2");
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.TabStop = false;
+            // 
+            // back
+            // 
+            this.back.BackColor = System.Drawing.Color.Transparent;
+            resources.ApplyResources(this.back, "back");
+            this.back.Name = "back";
+            this.back.TabStop = false;
+            // 
+            // bt_sign
+            // 
+            resources.ApplyResources(this.bt_sign, "bt_sign");
+            this.bt_sign.Name = "bt_sign";
+            this.bt_sign.UseVisualStyleBackColor = true;
+            this.bt_sign.Click += new System.EventHandler(this.bt_sign_Click);
+            // 
+            // bt_cancer
+            // 
+            resources.ApplyResources(this.bt_cancer, "bt_cancer");
+            this.bt_cancer.Name = "bt_cancer";
+            this.bt_cancer.UseVisualStyleBackColor = true;
+            // 
+            // Customer_Signup
+            // 
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.Controls.Add(this.bt_cancer);
+            this.Controls.Add(this.bt_sign);
+            this.Controls.Add(this.back);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.cus_telno);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.cus_name);
+            this.Controls.Add(this.pictureBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Customer_Signup";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.back)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TextBox cus_name;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.ComboBox cus_gender;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.ComboBox cus_type;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox cus_telno;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox back;
+        private System.Windows.Forms.Button bt_sign;
+        private System.Windows.Forms.Button bt_cancer;
+    }
+}
