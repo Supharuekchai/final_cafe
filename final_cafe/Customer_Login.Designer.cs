4d9c361622c8227e815dc462e9dca3da17cdb725
AsadaYoon <loveyou060842@gmail.com>
2020-04-19 16:59:49 +0700

    Customer all

---
+++ final_cafe/Customer_Login.Designer.cs
@@ -0,0 +1,205 @@
+﻿namespace final_cafe
+{
+    partial class Customer_Login
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Customer_Login));
+            this.cus_login = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.Customer_Password = new System.Windows.Forms.TextBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.Customer_ID = new System.Windows.Forms.TextBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.cus_signup = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // cus_login
+            // 
+            this.cus_login.AutoSize = true;
+            this.cus_login.BackColor = System.Drawing.Color.Transparent;
+            this.cus_login.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.cus_login.Font = new System.Drawing.Font("Lucida Bright", 12F, ((System.Drawing.FontStyle)(((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic) 
+                | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cus_login.Location = new System.Drawing.Point(94, 297);
+            this.cus_login.Name = "cus_login";
+            this.cus_login.Size = new System.Drawing.Size(54, 19);
+            this.cus_login.TabIndex = 49;
+            this.cus_login.Text = "Login";
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.panel2.Location = new System.Drawing.Point(101, 277);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(179, 1);
+            this.panel2.TabIndex = 47;
+            this.panel2.UseWaitCursor = true;
+            // 
+            // Customer_Password
+            // 
+            this.Customer_Password.BackColor = System.Drawing.Color.Linen;
+            this.Customer_Password.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.Customer_Password.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.Customer_Password.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(222)));
+            this.Customer_Password.Location = new System.Drawing.Point(99, 254);
+            this.Customer_Password.Name = "Customer_Password";
+            this.Customer_Password.PasswordChar = '*';
+            this.Customer_Password.Size = new System.Drawing.Size(181, 23);
+            this.Customer_Password.TabIndex = 43;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.panel1.Location = new System.Drawing.Point(101, 238);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(179, 1);
+            this.panel1.TabIndex = 46;
+            this.panel1.UseWaitCursor = true;
+            // 
+            // Customer_ID
+            // 
+            this.Customer_ID.BackColor = System.Drawing.Color.Linen;
+            this.Customer_ID.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.Customer_ID.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.Customer_ID.Font = new System.Drawing.Font("Lucida Bright", 15F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Customer_ID.Location = new System.Drawing.Point(101, 212);
+            this.Customer_ID.Name = "Customer_ID";
+            this.Customer_ID.Size = new System.Drawing.Size(178, 24);
+            this.Customer_ID.TabIndex = 45;
+            this.Customer_ID.Text = "USER ID";
+            this.Customer_ID.TextChanged += new System.EventHandler(this.Staff_Code_TextChanged);
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox3.BackgroundImage")));
+            this.pictureBox3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox3.Location = new System.Drawing.Point(61, 247);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(34, 31);
+            this.pictureBox3.TabIndex = 48;
+            this.pictureBox3.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox2.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox2.BackgroundImage")));
+            this.pictureBox2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox2.Location = new System.Drawing.Point(61, 208);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(34, 31);
+            this.pictureBox2.TabIndex = 44;
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.UseWaitCursor = true;
+            // 
+            // cus_signup
+            // 
+            this.cus_signup.AutoSize = true;
+            this.cus_signup.BackColor = System.Drawing.Color.Transparent;
+            this.cus_signup.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.cus_signup.Font = new System.Drawing.Font("Lucida Bright", 12F, ((System.Drawing.FontStyle)(((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic) 
+                | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cus_signup.Location = new System.Drawing.Point(183, 297);
+            this.cus_signup.Name = "cus_signup";
+            this.cus_signup.Size = new System.Drawing.Size(65, 19);
+            this.cus_signup.TabIndex = 50;
+            this.cus_signup.Text = "SignUp";
+            this.cus_signup.Click += new System.EventHandler(this.cus_signup_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.BackgroundImage")));
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.pictureBox1.Location = new System.Drawing.Point(89, 35);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(175, 147);
+            this.pictureBox1.TabIndex = 51;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox4.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox4.BackgroundImage")));
+            this.pictureBox4.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox4.Location = new System.Drawing.Point(310, 1);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(41, 42);
+            this.pictureBox4.TabIndex = 52;
+            this.pictureBox4.TabStop = false;
+            // 
+            // Customer_Login
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.BackgroundImage = global::final_cafe.Properties.Resources.pngtree_tea_restaurant_poster_background_material_image_172982;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(353, 480);
+            this.Controls.Add(this.pictureBox4);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.cus_signup);
+            this.Controls.Add(this.cus_login);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.Customer_Password);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.Customer_ID);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Customer_Login";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Customer_Login";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label cus_login;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.TextBox Customer_Password;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.TextBox Customer_ID;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Label cus_signup;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox4;
+    }
+}
---
+++ final_cafe/Customer_Login.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace final_cafe
+{
+    public partial class Customer_Login : Form
+    {
+        public Customer_Login()
+        {
+            InitializeComponent();
+        }
+
+        private void Staff_Code_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void cus_signup_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            Customer_Signup op_signup = new Customer_Signup();
+            op_signup.ShowDialog();
+        }
+    }
+}
