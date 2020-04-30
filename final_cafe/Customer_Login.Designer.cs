6a424786780f82ead3a291924cd5438ac3a5c1c0
DeadEye <supharuekchai.i@ku.th>
2020-04-30 09:12:42 +0700

    push to git

---
+++ final_cafe/Customer_Login.Designer.cs
@@ -0,0 +1,176 @@
+﻿namespace final_cafe
+{
+    partial class Customer_Login
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Customer_Login));
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.cus_signup = new System.Windows.Forms.Label();
+            this.cus_login = new System.Windows.Forms.Label();
+            this.Customer_ID = new System.Windows.Forms.TextBox();
+            this.close = new System.Windows.Forms.Label();
+            this.Customer_Password = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox4.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox4.BackgroundImage")));
+            this.pictureBox4.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox4.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.pictureBox4.Location = new System.Drawing.Point(-14, -13);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(84, 84);
+            this.pictureBox4.TabIndex = 62;
+            this.pictureBox4.TabStop = false;
+            this.pictureBox4.Click += new System.EventHandler(this.pictureBox4_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.BackgroundImage")));
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.pictureBox1.Location = new System.Drawing.Point(48, 32);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(275, 221);
+            this.pictureBox1.TabIndex = 61;
+            this.pictureBox1.TabStop = false;
+            // 
+            // cus_signup
+            // 
+            this.cus_signup.AutoSize = true;
+            this.cus_signup.BackColor = System.Drawing.Color.Transparent;
+            this.cus_signup.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.cus_signup.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cus_signup.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.cus_signup.Location = new System.Drawing.Point(171, 388);
+            this.cus_signup.Name = "cus_signup";
+            this.cus_signup.Size = new System.Drawing.Size(103, 31);
+            this.cus_signup.TabIndex = 60;
+            this.cus_signup.Text = "SignUp";
+            this.cus_signup.Click += new System.EventHandler(this.cus_signup_Click);
+            this.cus_signup.MouseEnter += new System.EventHandler(this.cus_signup_MouseEnter);
+            this.cus_signup.MouseLeave += new System.EventHandler(this.cus_signup_MouseLeave);
+            // 
+            // cus_login
+            // 
+            this.cus_login.AutoSize = true;
+            this.cus_login.BackColor = System.Drawing.Color.Transparent;
+            this.cus_login.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.cus_login.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cus_login.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.cus_login.Location = new System.Drawing.Point(44, 388);
+            this.cus_login.Name = "cus_login";
+            this.cus_login.Size = new System.Drawing.Size(80, 31);
+            this.cus_login.TabIndex = 59;
+            this.cus_login.Text = "Login";
+            this.cus_login.Click += new System.EventHandler(this.cus_login_Click);
+            this.cus_login.MouseEnter += new System.EventHandler(this.cus_login_MouseEnter);
+            this.cus_login.MouseLeave += new System.EventHandler(this.cus_login_MouseLeave);
+            // 
+            // Customer_ID
+            // 
+            this.Customer_ID.BackColor = System.Drawing.Color.White;
+            this.Customer_ID.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.Customer_ID.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Customer_ID.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.Customer_ID.Location = new System.Drawing.Point(42, 275);
+            this.Customer_ID.Name = "Customer_ID";
+            this.Customer_ID.Size = new System.Drawing.Size(232, 38);
+            this.Customer_ID.TabIndex = 55;
+            // 
+            // close
+            // 
+            this.close.AutoSize = true;
+            this.close.BackColor = System.Drawing.Color.Transparent;
+            this.close.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.close.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.close.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.close.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.close.Location = new System.Drawing.Point(279, 2);
+            this.close.Name = "close";
+            this.close.Size = new System.Drawing.Size(29, 27);
+            this.close.TabIndex = 63;
+            this.close.Tag = "";
+            this.close.Text = "X";
+            this.close.Click += new System.EventHandler(this.close_Click);
+            this.close.MouseEnter += new System.EventHandler(this.close_MouseEnter);
+            this.close.MouseLeave += new System.EventHandler(this.close_MouseLeave);
+            // 
+            // Customer_Password
+            // 
+            this.Customer_Password.BackColor = System.Drawing.Color.White;
+            this.Customer_Password.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.Customer_Password.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Customer_Password.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.Customer_Password.Location = new System.Drawing.Point(42, 319);
+            this.Customer_Password.Name = "Customer_Password";
+            this.Customer_Password.PasswordChar = '*';
+            this.Customer_Password.Size = new System.Drawing.Size(232, 38);
+            this.Customer_Password.TabIndex = 64;
+            // 
+            // Customer_Login
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::final_cafe.Properties.Resources._0b926ae748eab1a8c77bdb9cee6eb8c1;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(313, 479);
+            this.Controls.Add(this.Customer_Password);
+            this.Controls.Add(this.close);
+            this.Controls.Add(this.pictureBox4);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.cus_signup);
+            this.Controls.Add(this.cus_login);
+            this.Controls.Add(this.Customer_ID);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Customer_Login";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Customer_Login";
+            this.Load += new System.EventHandler(this.Customer_Login_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label cus_signup;
+        private System.Windows.Forms.Label cus_login;
+        private System.Windows.Forms.TextBox Customer_ID;
+        private System.Windows.Forms.Label close;
+        private System.Windows.Forms.TextBox Customer_Password;
+    }
+}
---
+++ final_cafe/ViewAllProucts.Designer.cs
@@ -0,0 +1,205 @@
+﻿namespace final_cafe
+{
+    partial class ViewAllProucts
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ProductCategoryComboBox = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.ProductsGroupBox = new System.Windows.Forms.GroupBox();
+            this.ProductsGridView = new System.Windows.Forms.DataGridView();
+            this.ProductIDColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.ProductNameColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.ProductPriceColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.ProductCategoryColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.ProductDescriptionColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.ProductImageColumn = new System.Windows.Forms.DataGridViewImageColumn();
+            this.EditProductColumn = new System.Windows.Forms.DataGridViewButtonColumn();
+            this.DeleteProductColumn = new System.Windows.Forms.DataGridViewButtonColumn();
+            this.close = new System.Windows.Forms.Label();
+            this.ProductsGroupBox.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.ProductsGridView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // ProductCategoryComboBox
+            // 
+            this.ProductCategoryComboBox.AutoCompleteMode = System.Windows.Forms.AutoCompleteMode.SuggestAppend;
+            this.ProductCategoryComboBox.AutoCompleteSource = System.Windows.Forms.AutoCompleteSource.ListItems;
+            this.ProductCategoryComboBox.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.ProductCategoryComboBox.FormattingEnabled = true;
+            this.ProductCategoryComboBox.Location = new System.Drawing.Point(100, 10);
+            this.ProductCategoryComboBox.Name = "ProductCategoryComboBox";
+            this.ProductCategoryComboBox.Size = new System.Drawing.Size(499, 21);
+            this.ProductCategoryComboBox.Sorted = true;
+            this.ProductCategoryComboBox.TabIndex = 5;
+            this.ProductCategoryComboBox.SelectedIndexChanged += new System.EventHandler(this.ProductCategoryComboBox_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(12, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(82, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Select Category";
+            // 
+            // ProductsGroupBox
+            // 
+            this.ProductsGroupBox.Controls.Add(this.ProductsGridView);
+            this.ProductsGroupBox.ForeColor = System.Drawing.Color.Black;
+            this.ProductsGroupBox.Location = new System.Drawing.Point(12, 34);
+            this.ProductsGroupBox.Name = "ProductsGroupBox";
+            this.ProductsGroupBox.Size = new System.Drawing.Size(612, 325);
+            this.ProductsGroupBox.TabIndex = 3;
+            this.ProductsGroupBox.TabStop = false;
+            this.ProductsGroupBox.Text = "Products";
+            // 
+            // ProductsGridView
+            // 
+            this.ProductsGridView.AllowUserToAddRows = false;
+            this.ProductsGridView.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.ProductsGridView.BackgroundColor = System.Drawing.Color.Chocolate;
+            this.ProductsGridView.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.ProductsGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.ProductsGridView.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.ProductIDColumn,
+            this.ProductNameColumn,
+            this.ProductPriceColumn,
+            this.ProductCategoryColumn,
+            this.ProductDescriptionColumn,
+            this.ProductImageColumn,
+            this.EditProductColumn,
+            this.DeleteProductColumn});
+            this.ProductsGridView.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.ProductsGridView.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.ProductsGridView.Location = new System.Drawing.Point(3, 16);
+            this.ProductsGridView.Name = "ProductsGridView";
+            this.ProductsGridView.Size = new System.Drawing.Size(606, 306);
+            this.ProductsGridView.TabIndex = 0;
+            this.ProductsGridView.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.ProductsGridView_CellContentClick);
+            // 
+            // ProductIDColumn
+            // 
+            this.ProductIDColumn.HeaderText = "ID";
+            this.ProductIDColumn.Name = "ProductIDColumn";
+            // 
+            // ProductNameColumn
+            // 
+            this.ProductNameColumn.HeaderText = "Product Name";
+            this.ProductNameColumn.Name = "ProductNameColumn";
+            // 
+            // ProductPriceColumn
+            // 
+            this.ProductPriceColumn.HeaderText = "Price";
+            this.ProductPriceColumn.Name = "ProductPriceColumn";
+            // 
+            // ProductCategoryColumn
+            // 
+            this.ProductCategoryColumn.HeaderText = "Category";
+            this.ProductCategoryColumn.Name = "ProductCategoryColumn";
+            // 
+            // ProductDescriptionColumn
+            // 
+            this.ProductDescriptionColumn.HeaderText = "Description";
+            this.ProductDescriptionColumn.Name = "ProductDescriptionColumn";
+            // 
+            // ProductImageColumn
+            // 
+            this.ProductImageColumn.HeaderText = "Image";
+            this.ProductImageColumn.ImageLayout = System.Windows.Forms.DataGridViewImageCellLayout.Stretch;
+            this.ProductImageColumn.Name = "ProductImageColumn";
+            this.ProductImageColumn.Resizable = System.Windows.Forms.DataGridViewTriState.True;
+            this.ProductImageColumn.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.Automatic;
+            // 
+            // EditProductColumn
+            // 
+            this.EditProductColumn.HeaderText = "Edit";
+            this.EditProductColumn.Name = "EditProductColumn";
+            this.EditProductColumn.Resizable = System.Windows.Forms.DataGridViewTriState.True;
+            this.EditProductColumn.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.Automatic;
+            // 
+            // DeleteProductColumn
+            // 
+            this.DeleteProductColumn.HeaderText = "Delete";
+            this.DeleteProductColumn.Name = "DeleteProductColumn";
+            // 
+            // close
+            // 
+            this.close.AutoSize = true;
+            this.close.BackColor = System.Drawing.Color.Transparent;
+            this.close.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.close.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.close.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.close.Location = new System.Drawing.Point(605, 6);
+            this.close.Name = "close";
+            this.close.Size = new System.Drawing.Size(19, 18);
+            this.close.TabIndex = 65;
+            this.close.Tag = "";
+            this.close.Text = "X";
+            this.close.Click += new System.EventHandler(this.close_Click);
+            this.close.MouseEnter += new System.EventHandler(this.close_MouseEnter);
+            this.close.MouseLeave += new System.EventHandler(this.close_MouseLeave);
+            // 
+            // ViewAllProucts
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.SaddleBrown;
+            this.ClientSize = new System.Drawing.Size(631, 367);
+            this.Controls.Add(this.close);
+            this.Controls.Add(this.ProductCategoryComboBox);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.ProductsGroupBox);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "ViewAllProucts";
+            this.Text = "ViewAllProucts";
+            this.Load += new System.EventHandler(this.ViewAllProucts_Load);
+            this.ProductsGroupBox.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.ProductsGridView)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox ProductCategoryComboBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox ProductsGroupBox;
+        private System.Windows.Forms.DataGridView ProductsGridView;
+        private System.Windows.Forms.Label close;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ProductIDColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ProductNameColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ProductPriceColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ProductCategoryColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ProductDescriptionColumn;
+        private System.Windows.Forms.DataGridViewImageColumn ProductImageColumn;
+        private System.Windows.Forms.DataGridViewButtonColumn EditProductColumn;
+        private System.Windows.Forms.DataGridViewButtonColumn DeleteProductColumn;
+    }
+}
